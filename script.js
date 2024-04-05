7f51cb61dcd032acf81b4f34e5d25504c22c80fd
Arsh Ilahi <156427743+Arsh249@users.noreply.github.com>
2024-04-06 00:01:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 function firstWord(s) {
   if(s.indexOf(' ') == -1 || s.length == 0)
 	  return s;
+	if(s[0]==" "){
+		let str = s.split(" ")[1];
+		return str;
+	}
  let str = s.split(" ")[0];
 	return str;
 }
-  
+
 // Do not change the code below
  
 const s = prompt("Enter String:");
